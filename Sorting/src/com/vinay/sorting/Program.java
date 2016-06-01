93ac6609b5ed763081a43a996751790dd57279eb
Vinay Kc <Vinay Kc>
2016-06-01 20:16:11 +0545

    Sorting

--- Sorting/src/com/vinay/sorting/Program.java
+++ Sorting/src/com/vinay/sorting/Program.java
@@ -10,12 +10,24 @@ package com.vinay.sorting;
  * @author Leviathan
  */
 public class Program {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int[] num={2,1,5,15,8,16,12,1};
+    
+    public static void asc(int[] num){
+        for(int i=0;i<num.length;i++){
+            for(int j=i+1;j<num.length;j++){
+                if(num[i]>num[j]){
+                    int temp=num[i];
+                    num[i]=num[j];
+                    num[j]=temp;
+                }
+            }
+        }
+        System.out.println("In ascending order: ");
+        for(int i=0;i<num.length;i++){
+        System.out.println(num[i]);
+        }
+    }
+    
+    public static void des(int[] num){
         for(int i=0;i<num.length;i++){
             for(int j=i+1;j<num.length;j++){
                 if(num[i]<num[j]){
@@ -25,11 +37,19 @@ public class Program {
                 }
             }
         }
-        
-        System.out.println("In descending order: ");
+        System.out.println("\nIn descending order: ");
         for(int i=0;i<num.length;i++){
             System.out.println(num[i]);
         }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] num={2,1,5,15,8,16,12,1};
+        asc(num);
+        des(num);
         
 // TODO code application logic here
     }
